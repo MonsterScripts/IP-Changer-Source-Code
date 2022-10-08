afb42832b9f2e2b7309b558b141716e67bd5aabc
Monster <97206566+MonsterScripts@users.noreply.github.com>
2022-10-08 22:08:46 +0200

    Update changeip.bat

--- changeip.bat
+++ changeip.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFDxgfyXCcSuTCLMV5uHv9tajrVoTWO0+fJzn3rGeL/Ma/lXseoIixUUIzZldXEwBLkTmfRs7qmUPv2iJItSIoQ71Q0eHq101GHF7lS7ZgCcuc8d6m9cK3C7z+0Lzm6AenHT6Sr0LB3CvyK9nWA==
-::YAwzuBVtJxjWCl3EqQJhSA==
-::ZR4luwNxJguZRRnVoxNncEk0
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFDxgfyXCcSuTCLMV5uHv9tajrVoTWO0+fJzn+byPLucW41WqcI4otg==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 Echo ipconfig/flushdns
 ipconfig/flushdns
 echo ipconfig/release
